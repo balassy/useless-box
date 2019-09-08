05d24c59b87f726760f7ceb5fb767a6ce06d6739
György Balássy <balassy@users.noreply.github.com>
2019-09-09 00:42:45 +0200

    FIXED: Slow servo rotation is also made in steps.

--- speed-servo.cpp
+++ speed-servo.cpp
@@ -1,28 +1,39 @@
 #include "speed-servo.h"
 
 const int SLOW_MOVE_DELAY = 15;
+const int Fast_MOVE_DELAY = 2;
 
 void SpeedServo::attach(uint8_t pin) {
   _servo.attach(pin);
 }
 
 // Valid position: 0-180.
-void SpeedServo::moveFastTo(int newPosition) {
+void SpeedServo::moveNowTo(int newPosition) {
   _lastPosition = newPosition;
   _servo.write(newPosition);
 }
 
 // Valid position: 0-180.
+void SpeedServo::moveFastTo(int newPosition) {
+  _moveTo(newPosition, Fast_MOVE_DELAY);
+}
+
+// Valid position: 0-180.
 void SpeedServo::moveSlowTo(int newPosition) {
+  _moveTo(newPosition, SLOW_MOVE_DELAY);
+}
+
+// Valid position: 0-180.
+void SpeedServo::_moveTo(int newPosition, unsigned long stepDelay) {
   if(newPosition > _lastPosition) {
     for (int pos = _lastPosition; pos <= newPosition; pos++) {
       _servo.write(pos);
-      delay(SLOW_MOVE_DELAY);
+      delay(stepDelay);
     }
   } else {
     for (int pos = _lastPosition; pos >= newPosition; pos--) {
       _servo.write(pos);
-      delay(SLOW_MOVE_DELAY);
+      delay(stepDelay);
     }
   }
 
--- speed-servo.h
+++ speed-servo.h
@@ -7,12 +7,14 @@
 class SpeedServo {
   public:
     void attach(uint8_t pin);
+    void moveNowTo(int position);
     void moveFastTo(int position);
     void moveSlowTo(int position);
 
   private:
     Servo _servo;
     int _lastPosition;
+    void _moveTo(int newPosition, unsigned long stepDelay);
 };
 
 #endif /* speed_servo_h */
